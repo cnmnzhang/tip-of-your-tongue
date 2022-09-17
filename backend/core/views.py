978f9a25a229d29974426933154912a45dbc6a17
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-09-17 18:55:42 -0400

    added another route to backend

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from core import app
+from datetime import datetime
+import re
 
 @app.route('/')
 def index():
@@ -13,3 +15,20 @@ def index():
 
     # greeting = 'Hello Ace'
     # return render_template('index.html', greet=greeting)
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello there, " + clean_name + "! It's " + formatted_now
+    return content

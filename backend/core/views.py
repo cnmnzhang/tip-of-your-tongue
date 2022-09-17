7b78fadbb4f2e0e83533590893d7ef3a5c5c7478
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-09-17 14:53:49 -0400

    running front end with filter search and running back end

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,5 +1,15 @@
+from flask import render_template
 from core import app
 
 @app.route('/')
 def index():
-    return 'Hello Ace'
+
+    response_body = {
+        "name": "Nagato",
+        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+    }
+
+    return response_body
+
+    # greeting = 'Hello Ace'
+    # return render_template('index.html', greet=greeting)

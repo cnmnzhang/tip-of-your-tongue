334b2d1546a6d22a29f3731f1cf8b399e3c6b433
czhan117 <59889383+czhan117@users.noreply.github.com>
2022-09-17 20:04:30 -0400

    updated background color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,32 @@
+import React, { useState} from 'react';
 import cat from './cat.svg';
 import Search from './components/Search';
+import axios from "axios";
+
 
 function App() {
+
+  // new line start
+  const [profileData, setProfileData] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"/profile",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })};
+    //end of new line 
 
   return (
     <div className="App">
@@ -19,6 +44,15 @@ function App() {
 
       <div className="App-body">
         <Search />
+
+        {/* new line start*/}
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
+         {/* end of new line */}
 
 
       </div>
